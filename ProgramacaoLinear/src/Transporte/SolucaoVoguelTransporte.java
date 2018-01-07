2ec9bc9b39866033594ae066be56b8e7993ff279
Unknown <nikollas_gabriel@hotmail.com>
2018-01-07 16:16:39 -0200

    Voguel v1.0

--- ProgramacaoLinear/src/Transporte/SolucaoVoguelTransporte.java
+++ ProgramacaoLinear/src/Transporte/SolucaoVoguelTransporte.java
@@ -1,28 +1,73 @@
-<<<<<<< HEAD
 /**
  * *********************************************************
  * ALUNOS: NIKOLLAS G. ASSUMPÇÂO & RENAN WINTER SPATIN *
  ***********************************************************
  */
-
 package Transporte;
 
-public class SolucaoVoguelTransporte {
-    
-    
-=======
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package Transporte;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 
-/**
- *
- * @author asus note
- */
 public class SolucaoVoguelTransporte {
-    
->>>>>>> master
+
+    public TabelaTransporte calcularPenalidades(TabelaTransporte tabela) {
+
+    }
+
+    public void calcularPenalidadeLinha(TabelaTransporte tabela) {
+
+        Float[][] oferta = tabela.getCustosTransporte();
+
+        for (int linha = 0; linha < tabela.getTamanhoOfertas(); linha++) {
+
+            ArrayList<Float> valores = new ArrayList<>();
+
+            valores.addAll(Arrays.asList(oferta[linha]));
+
+            tabela.getPenalidadesOferta()[linha] = calcularDiferenca(getDoisMenores(valores));
+
+        }
+
+    }
+
+    public void calcularPenalidadeColuna(TabelaTransporte tabela) {
+
+        Float[][] demanda = tabela.getCustosTransporte();
+
+        for (int coluna = 0; coluna < tabela.getTamanhoDemandas(); coluna++) {
+
+            for (int linha = 0; linha < tabela.getTamanhoOfertas(); coluna++) {
+
+                ArrayList<Float> valores = new ArrayList<>();
+
+                valores.add(demanda[linha][coluna]);
+
+                tabela.getPenalidadesDemanda()[coluna] = calcularDiferenca(getDoisMenores(valores));
+
+            }
+
+        }
+
+    }
+
+    public ArrayList<Float> getDoisMenores(ArrayList<Float> numeros) {
+
+        ArrayList<Float> x = new ArrayList<>();
+
+        Collections.sort(numeros);
+
+        x.add(numeros.get(0));
+        x.add(numeros.get(1));
+
+        return x;
+
+    }
+
+    public Float calcularDiferenca(ArrayList<Float> valores) {
+
+        return (valores.get(1) - valores.get(0));
+
+    }
+
 }
